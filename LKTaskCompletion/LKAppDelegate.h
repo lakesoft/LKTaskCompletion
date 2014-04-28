100b4d07682c7440e11e7d61605c7329bf150a99
Hiroshi Hashiguchi <xcatsan@mac.com>
2014-04-29 08:31:48 +0900

    Initial Commit

---
+++ LKTaskCompletion/LKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LKAppDelegate.h
+//  LKTaskCompletion
+//
+//  Created by Hiroshi Hashiguchi on 2014/04/29.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LKTaskCompletion/LKViewController.h
@@ -0,0 +1,13 @@
+//
+//  LKViewController.h
+//  LKTaskCompletion
+//
+//  Created by Hiroshi Hashiguchi on 2014/04/29.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKViewController : UIViewController
+
+@end
